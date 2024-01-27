2fbf7a932d7d491e8bc4a43ed6d98b15eb7cda4c
FlexProdD <112783610+FlexProdD@users.noreply.github.com>
2024-01-27 16:25:57 +0300

    Add files via upload

---
+++ switchcase.swift
@@ -0,0 +1,24 @@
+
+import Foundation
+
+print("1.AVUSTURYA")
+print("2.FRANSA")
+print("3.İTALYA")
+print("4.HIRVATİSTAN")
+
+var n = 2
+
+switch n {
+    case 1:
+        print("AVUSTURYAYI SEÇTİNİZ")
+    case 2:
+        print("FRANSAYI SEÇTİNİZ")
+    case 3:
+        print("İTALYAYI SEÇTİNİZ")
+    case 4:
+        print("HIRVATİSTANI SEÇTİNİZ")
+    default:
+        print("HATALI SEÇİM")
+}
+
+
